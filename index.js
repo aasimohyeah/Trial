64476b76dd542739ef1c0e9609a649a5369c9b4d
aasimohyeah <aasimohyeah@gmail.com>
2024-10-04 23:46:59 +0530

    external module added to Node farm

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
+//file reader
 const fs = require('fs');
+//SERVER
+const http = require('http');
+const url = require('url');
+//custom module
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //function replacetemplate 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic)
-    {
-        output = output.replace(/{%NOT_ORGANIC}/g, 'not-organic');
-    }
-    return output;
-}
+
 
 const tempOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8');
 const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
@@ -24,10 +16,6 @@ const tempProduct = fs.readFileSync('./templates/template-product.html', 'utf-8'
 //${__dirname} not working, so using . instead
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
-
-//SERVER
-const http = require('http');
-const url = require('url');
 
 const server = http.createServer((req, res) => {
     //res.end('Hello from the server\nGae');
