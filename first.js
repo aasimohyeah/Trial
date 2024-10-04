ebcbf0057fbdd09e4932665095899fa1d63dbeb7
aasimohyeah <aasimohyeah@gmail.com>
2024-10-04 23:18:57 +0530

    Node farm project completed

--- first.js
+++ first.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+//function replacetemplate 
 const replaceTemplate = (temp, product) => {
     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
@@ -31,30 +32,38 @@ const url = require('url');
 const server = http.createServer((req, res) => {
     //res.end('Hello from the server\nGae');
 
-    //to see number of request made by website
+    //to see number of request made by website use
     //console.log(req.url);
+    //to see explanation of url.parse use
+    //console.log(url.parse(req.url, true));
 
-    const pathName = req.url;
+    const {query, pathname} = url.parse(req.url, true);// query and pathname fetched 
+    //using url.parse(req.url ,true) and then indexing on the result
     
     //implemented basic routing below
-
     //Overview Page
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {
             'Content-type': 'text/html'
-        })
+        });
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        //.join used to join all output strings into one
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
         res.end(output);
     }
     //Product Page
-    else if(pathName === '/product'){
-        res.end('This is the PRODUCT');
+    else if(pathname === '/product'){
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
     //API
-    else if(pathName == '/api'){
+    else if(pathname == '/api'){
         res.writeHead(200, {
             'Content-type': 'application/json'
         })
